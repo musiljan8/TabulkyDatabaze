b79df42004918a7e6e631f418b746176ac1bbc2a
honzamusil <honzamusil@localhost>
2017-03-21 19:17:43 +0100

    seznam jede, form_send se mi to nezapise

--- TabulkyDatabaze/form_sent.php
+++ TabulkyDatabaze/form_sent.php
@@ -2,21 +2,21 @@
 
  <?php
 
-                require_once('Db.php');
-                Db::connect('127.0.0.1', 'moje_databaze', 'root', '');
-                if ($_GET)
-                {
+    require_once('Db.php');
+    Db::connect('127.0.0.1', 'moje_databaze', 'root', '');
+    if ($_GET)
+    {
                         
-                        Db::query('
-                                INSERT INTO uzivatele (email, jmeno, prijmeni)
-                                VALUES (?, ?, ?)
-                        ', $_GET['email'], $_GET['jmeno'],$_GET['prijmeni'] );
+    Db::query('
+        INSERT INTO uzivatele (email, jmeno, prijmeni)
+        VALUES (?, ?, ?)
+     ', $_GET['email'], $_GET['jmeno'],$_GET['prijmeni'] );
 
-                        echo('<p>Byl jste zaregistrován.</p>');
-                }
+        echo('<p>Byl jste zaregistrován.</p>');
+    }
      
-
 ?>
+
  
 Dusan Drak
 pepa@pepa.czPepa Nos
@@ -24,9 +24,7 @@ pepa@pepa.czPepa Nos
 pepa@pepa.czPepa Nos
 pepa@pepa.czPepa Nos
 john@johnJonh Lennon
-john@johnJonh Lennon
-john@johnJonh Lennon
-john@johnJonh Lennon
+
 john@johnJonh Lennon
 alois@alois.czAlois Slama
 alois@alois.czAlois Slama
